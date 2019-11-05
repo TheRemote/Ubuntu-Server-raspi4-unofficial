dc78a37841347f921dec41fddaa8fff85fc836a8
James A. Chambers <05jchambers@gmail.com>
2019-11-04 21:24:59 -0700

    v19 Development

--- conform_config_jamesachambers.sh
+++ conform_config_jamesachambers.sh
@@ -79,3 +79,23 @@ set_kernel_config CONFIG_NETFILTER_XT_TARGET_AUDIT m
 
 # Enable YAMA/Ptrace
 set_kernel_config CONFIG_SECURITY_YAMA y
+
+# Crypto
+set_kernel_config CONFIG_CRYPTO_ENGINE m
+set_kernel_config CONFIG_CRYPTO_SHA256_ARM64 m
+set_kernel_config CONFIG_CRYPTO_SHA512_ARM64 m
+set_kernel_config CONFIG_CRYPTO_SHA1_ARM64_CE m
+set_kernel_config CONFIG_CRYPTO_SHA2_ARM64_CE m
+set_kernel_config CONFIG_CRYPTO_GHASH_ARM64_CE m
+set_kernel_config CONFIG_CRYPTO_CRCT10DIF_ARM64_CE m
+set_kernel_config CONFIG_CRYPTO_AES_ARM64_CE m
+set_kernel_config CONFIG_CRYPTO_AES_ARM64_CE_CCM m
+set_kernel_config CONFIG_CRYPTO_AES_ARM64_CE_BLK m
+set_kernel_config CONFIG_CRYPTO_CHACHA20_NEON m
+
+# Security
+set_kernel_config CONFIG_PERSISTENT_KEYRINGS y
+set_kernel_config ONFIG_BIG_KEYS y
+set_kernel_config CONFIG_TRUSTED_KEYS y
+set_kernel_config CONFIG_ENCRYPTED_KEYS y
+set_kernel_config CONFIG_SECURITY y
