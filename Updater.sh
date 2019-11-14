109749eab3f33ebddcdfe0defad91a206ca53b46
Artyom Aleksyuk <artyom.h31@gmail.com>
2019-11-14 08:24:29 +0300

    Undo changes in Updater.sh

--- Updater.sh
+++ Updater.sh
@@ -4,6 +4,14 @@
 # https://jamesachambers.com/raspberry-pi-4-ubuntu-server-desktop-18-04-3-image-unofficial/
 # https://github.com/TheRemote/Ubuntu-Server-raspi4-unofficial
 
+# Check for sudo
+if [ -z "$SUDO_USER" ]; then
+    echo "Error: must run as sudo (sudo ./Updater.sh)"
+    exit
+fi
+
+echo "Checking dependencies ..."
+
 # Add updated mesa repository for video driver support
 sudo add-apt-repository ppa:ubuntu-x-swat/updates -ynr
 sudo add-apt-repository ppa:ubuntu-raspi2/ppa -ynr
@@ -15,11 +23,94 @@ if [ -e /etc/modules-load.d/cups-filters.conf ]; then
   systemctl restart systemd-modules-load cups
 fi
 
+# Install dependencies
+sudo apt update && sudo apt install libblockdev-mdraid2 wireless-tools iw rfkill bluez haveged libnewt0.52 whiptail lua5.1 git bc bison flex libssl-dev -y
+sudo apt-get dist-upgrade -y
+
+echo "Checking for updates ..."
+
+if [ -d ".updates" ]; then
+    cd .updates
+    if [ -d "Ubuntu-Server-raspi4-unofficial" ]; then
+        cd Ubuntu-Server-raspi4-unofficial
+        sudo git pull
+        sudo git reset --hard origin/master
+        cd ..
+    else
+        sudo git clone https://github.com/TheRemote/Ubuntu-Server-raspi4-unofficial.git
+    fi
+else
+    sudo mkdir .updates
+    cd .updates
+    sudo git clone https://github.com/TheRemote/Ubuntu-Server-raspi4-unofficial.git
+fi
+cd ..
+
+# Check if Updater.sh has been updated
+UpdatesHashOld=$(sha1sum "Updater.sh" | cut -d" " -f1 | xargs)
+UpdatesHashNew=$(sha1sum ".updates/Ubuntu-Server-raspi4-unofficial/Updater.sh" | cut -d" " -f1 | xargs)
+
+if [ "$UpdatesHashOld" != "$UpdatesHashNew" ]; then
+    echo "Updater has update available.  Updating now ..."
+    sudo rm -f Updater.sh
+    sudo cp -f .updates/Ubuntu-Server-raspi4-unofficial/Updater.sh Updater.sh
+    sudo chmod +x Updater.sh
+    exec $(readlink -f "Updater.sh")
+    exit
+fi
+
+echo "Updater is up to date.  Checking system ..."
+
+# Find currently installed and latest release
+cd .updates
+LatestRelease=$(cat "Ubuntu-Server-raspi4-unofficial/BuildPiKernel64bit.sh" | grep "IMAGE_VERSION=" | cut -d"=" -f2 | xargs)
+CurrentRelease="0"
+
+if [ -e "/etc/imgrelease" ]; then
+    read -r CurrentRelease < "/etc/imgrelease"
+fi
+
+if [ "$LatestRelease" == "$CurrentRelease" ]; then
+    echo "You have release ${LatestRelease}. No updates are currently available!"
+    exit
+fi
+
+# Update is available, confirm insatallation with user
+echo "Release v${LatestRelease} is available!  Make sure you have made a full backup."
 echo "Note: your /boot cmdline.txt and config.txt files will be reset to the newest version.  Make a backup of those first!"
+echo -n "Update now? (y/n)"
+read answer
+if [ "$answer" == "${answer#[Yy]}" ]; then
+    echo "Update has been aborted!"
+    exit
+fi
+
+# Cleaning up old stuff
+sudo apt purge libraspberrypi-bin raspi-config -y
+
+echo "Downloading update package ..."
+if [ -e "updates.tar.xz" ]; then rm -f "updates.tar.xz"; fi
+sudo curl --location "https://github.com/TheRemote/Ubuntu-Server-raspi4-unofficial/releases/download/v${LatestRelease}/updates.tar.xz" --output "updates.tar.xz"
+if [ ! -e "updates.tar.xz" ]; then
+    echo "Update has failed to download -- please try again later"
+    exit
+fi
+
+# Download was successful, extract and copy updates
+echo "Extracting update package - This can take several minutes on the Pi ..."
+sudo rm -rf updates
+sudo tar -xf "updates.tar.xz"
+
+if [[ -d "updates" && -d "updates/rootfs" && -d "updates/bootfs" ]]; then
+    echo "Copying updates to rootfs ..."
+    sudo cp --verbose --archive --no-preserve=ownership updates/rootfs/* /
+
+    echo "Copying updates to bootfs ..."
+    sudo cp --verbose --archive --no-preserve=ownership updates/bootfs/* /boot/firmware
 
     # Update initramfs so our new kernel and modules are picked up
     echo "Updating kernel and modules ..."
-    export KERNEL_VERSION="$(ls -1 /lib/modules/ | grep v8 | sort -V | tail -n 1)"
+    export KERNEL_VERSION="$(ls updates/rootfs/lib/modules)"
     sudo depmod "${KERNEL_VERSION}"
 
     # Create kernel and component symlinks
@@ -45,11 +136,24 @@ echo "Note: your /boot cmdline.txt and config.txt files will be reset to the new
     echo "$sha1sum  /boot/vmlinux-${KERNEL_VERSION}" | sudo tee /var/lib/initramfs-tools/"${KERNEL_VERSION}" >/dev/null;
     sudo update-initramfs -k "${KERNEL_VERSION}" -u
 
+    echo "Cleaning up downloaded files ..."
+    sudo rm -f "updates.tar.xz"
+    sudo rm -rf updates
+
+    # Save our new updated release to .lastupdate file
+    sudo touch /etc/imgrelease
+    echo "$LatestRelease" | sudo tee /etc/imgrelease >/dev/null;
+else
+    sudo rm -f "updates.tar.xz"
+    echo "Update has failed to extract.  Please try again later!"
+    exit
+fi
+
 # % Add various groups to account such as the video group to allow access to vcgencmd and other userland utilities
 sudo groupadd -f spi
 sudo groupadd -f i2c
 sudo groupadd -f gpio
-#sudo usermod -aG adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio "$SUDO_USER"
+sudo usermod -aG adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio "$SUDO_USER"
 
 # % Clear /var/crash
 sudo rm -rf /var/crash/*
@@ -71,12 +175,12 @@ sudo sed -i "s/iscsi_tcp/#iscsi_tcp/g" /lib/modules-load.d/open-iscsi.conf
 sudo echo "SUBSYSTEM==\"vchiq\", GROUP=\"video\", MODE=\"0660\"" > /etc/udev/rules.d/10-local-rpi.rules
 
 # % Fix update-initramfs mdadm.conf warning
-#sudo grep "ARRAY devices" /etc/mdadm/mdadm.conf >/dev/null || echo "ARRAY devices=/dev/sda" | sudo tee -a /etc/mdadm/mdadm.conf >/dev/null;
+sudo grep "ARRAY devices" /etc/mdadm/mdadm.conf >/dev/null || echo "ARRAY devices=/dev/sda" | sudo tee -a /etc/mdadm/mdadm.conf >/dev/null;
 
 # Startup tweaks to fix bluetooth and sound issues
 sudo rm /etc/rc.local
 sudo touch /etc/rc.local
-cat << EOF | sudo tee /etc/rc.local
+cat << \EOF | sudo tee /etc/rc.local
 #!/bin/bash
 #
 # rc.local
@@ -171,6 +275,21 @@ EOF
     sudo netplan generate 
     sudo netplan --debug apply
 fi
+
+# Add proposed apt archive
+GrepCheck=$(cat /etc/apt/sources.list | grep "ubuntu-ports bionic-proposed")
+if [ -z "$GrepCheck" ]; then
+    cat << EOF | sudo tee -a /etc/apt/sources.list
+deb http://ports.ubuntu.com/ubuntu-ports bionic-proposed restricted main multiverse universe
+EOF
+fi
+
+sudo touch /etc/apt/preferences.d/proposed-updates 
+cat << EOF | sudo tee /etc/apt/preferences.d/proposed-updates
+Package: *
+Pin: release a=bionic-proposed
+Pin-Priority: 400
+EOF
 
 echo "Update completed!"
 echo "Note: it is recommended to periodically clean out the old kernel source from /usr/src, it's quite large!"
